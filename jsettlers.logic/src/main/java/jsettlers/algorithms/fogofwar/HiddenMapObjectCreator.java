6ab49afbfd7d5e59327d354ae258b050123faf21
Paul Wedeck <paulwedeck@gmail.com>
2021-06-20 14:02:54 +0200

    add blacklist of map object that should be hidden by the fog of war

--- jsettlers.logic/src/main/java/jsettlers/algorithms/fogofwar/HiddenMapObjectCreator.java
+++ jsettlers.logic/src/main/java/jsettlers/algorithms/fogofwar/HiddenMapObjectCreator.java
@@ -1,9 +1,30 @@
 package jsettlers.algorithms.fogofwar;
 
+import jsettlers.common.mapobject.EMapObjectType;
 import jsettlers.logic.map.grid.objects.AbstractHexMapObject;
 
-class HiddenMapObjectCreator {
+public class HiddenMapObjectCreator {
 	public static HiddenMapObject create(AbstractHexMapObject object) {
+		EMapObjectType type = object.getObjectType();
+		if(!type.persistent) return null;
+
+		switch (type) {
+			case ARROW: // TODO
+			case BUILDING: // TODO
+			case PLACEMENT_BUILDING:
+			case SMOKE:
+			case PIG:
+			case DONKEY:
+			case GHOST:
+			case STACK_OBJECT: // TODO
+			case BUILDING_DECONSTRUCTION_SMOKE:
+			case ATTACKABLE_TOWER:
+			case INFORMABLE_MAP_OBJECT:
+			case EYE:
+			case SPELL_EFFECT:
+				return null;
+		}
+
 		return new HiddenMapObject(object);
 	}
 }
