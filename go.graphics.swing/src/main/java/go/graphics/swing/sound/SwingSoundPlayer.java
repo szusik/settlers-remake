4fe589743cfbcf0c50c6e5465a91f98b2696b002
Paul Wedeck <paulwedeck@gmail.com>
2021-06-26 15:07:48 +0200

    add openal backend to go.graphics.swing

--- go.graphics.swing/src/main/java/go/graphics/swing/sound/SwingSoundPlayer.java
+++ go.graphics.swing/src/main/java/go/graphics/swing/sound/SwingSoundPlayer.java
@@ -16,118 +16,86 @@ package go.graphics.swing.sound;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import go.graphics.sound.ForgettingQueue;
-import go.graphics.sound.ForgettingQueue.Sound;
 import go.graphics.sound.ISoundDataRetriever;
 import go.graphics.sound.SoundHandle;
 import go.graphics.sound.SoundPlayer;
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.AL10;
+import org.lwjgl.openal.ALC;
+import org.lwjgl.openal.ALC10;
 
 public class SwingSoundPlayer implements SoundPlayer {
 	private static final int BUFFER_SIZE = 4048 * 4;
-	private static final int SOUND_THREADS = 30;
 
 	private final ISoundSettingsProvider soundSettingsProvider;
-	private ForgettingQueue<Integer> queue = new ForgettingQueue<>();
 	private ISoundDataRetriever soundDataRetriever;
+	private final long device;
+	private final long context;
+	private final List<Integer> sources = new ArrayList<>();
+	private final Map<Integer, Integer> sounds = new HashMap<>();
 
 	public SwingSoundPlayer(ISoundSettingsProvider soundSettingsProvider) {
 		this.soundSettingsProvider = soundSettingsProvider;
-		ThreadGroup soundGroup = new ThreadGroup("soundplayer");
-		for (int i = 0; i < SOUND_THREADS; i++) {
-			new Thread(soundGroup, new SoundPlayerTask(), "soundplayer" + i).start();
+		device = ALC10.alcOpenDevice((String)null);
+		context = ALC10.alcCreateContext(device, (IntBuffer)null);
+		ALC10.alcMakeContextCurrent(context);
+		AL.createCapabilities(ALC.createCapabilities(device));
+	}
+	private int getAvailableSource() {
+		for(int source : sources) {
+			if(AL10.alGetSourcei(source, AL10.AL_SOURCE_STATE) != AL10.AL_PLAYING) return source;
 		}
+
+		int newSource = AL10.alGenSources();
+		sources.add(newSource);
+		return newSource;
+	}
+
+	private int createSoundBuffer(int soundIndex) {
+		int buffer = AL10.alGenBuffers();
+		try {
+			short[] data = soundDataRetriever.getSoundData(soundIndex);
+			AL10.alBufferData(buffer, AL10.AL_FORMAT_MONO16, data, 20500);
+		} catch (IOException e) {
+			System.err.println("Could not create sound buffer for: " + soundIndex);
+			e.printStackTrace();
+		}
+		return buffer;
 	}
 
 	@Override
 	public void playSound(int soundStart, float leftVolume, float rightVolume) {
 		float gameVolume = soundSettingsProvider.getVolume();
-		leftVolume *= gameVolume;
-		rightVolume *= gameVolume;
 
-		if (leftVolume > 0 || rightVolume > 0) {
-			queue.offer(soundStart, leftVolume, rightVolume);
-		}
+		int source = getAvailableSource();
+
+		int buffer = sounds.computeIfAbsent(soundStart, this::createSoundBuffer);
+
+		float localVolume = (leftVolume+rightVolume)/2;
+		float globalVolume = gameVolume * localVolume;
+		AL10.alSourceRewind(source);
+		AL10.alSourcei(source, AL10.AL_BUFFER, buffer);
+		AL10.alSourcef(source, AL10.AL_GAIN, globalVolume);
+		AL10.alSource3f(source, AL10.AL_POSITION, (rightVolume-leftVolume)/localVolume, 0f, 0f);
+		AL10.alSourcePlay(source);
 	}
 
-	private byte[] transformData(short[] data) {
-		byte[] buffer = new byte[data.length * 4];
-		for (int i = 0; i < data.length; i++) {
-			buffer[4 * i] = buffer[4 * i + 2] = (byte) data[i];
-			buffer[4 * i + 1] = buffer[4 * i + 3] = (byte) (data[i] >> 8);
-		}
-
-		return buffer;
-	}
-
-	private byte[] transformData(short[] data, float l, float r) {
-		byte[] buffer = new byte[data.length * 4];
-		for (int i = 0; i < data.length; i++) {
-			int ld = (int) (data[i] * l);
-			buffer[4 * i] = (byte) ld;
-			buffer[4 * i + 1] = (byte) (ld >> 8);
-			int rd = (int) (data[i] * r);
-			buffer[4 * i + 2] = (byte) rd;
-			buffer[4 * i + 3] = (byte) (rd >> 8);
-		}
-
-		return buffer;
-	}
-
-	private class SoundPlayerTask implements Runnable {
-
-		@Override
-		public void run() {
-			AudioFormat format = new AudioFormat(22050, 16, 2, true, false);
-
-			Line.Info info = new Line.Info(SourceDataLine.class);
-
-			try {
-				SourceDataLine dataLine = (SourceDataLine) AudioSystem.getMixer(null).getLine(info);
-				dataLine.open(format, BUFFER_SIZE);
-
-				while (true) {
-					try {
-						// start sound playing
-						dataLine.start();
-
-						Sound<Integer> sound = queue.take();
-
-						byte[] buffer;
-						if (dataLine.isControlSupported(FloatControl.Type.VOLUME) && dataLine.isControlSupported(FloatControl.Type.BALANCE)) {
-							buffer = transformData(soundDataRetriever.getSoundData(sound.getData()));
-							FloatControl volumeControl = (FloatControl) dataLine.getControl(FloatControl.Type.VOLUME);
-							volumeControl.setValue(sound.getVolume() * volumeControl.getMaximum());
-							((FloatControl) dataLine.getControl(FloatControl.Type.BALANCE)).setValue(sound.getBalance());
-						} else {
-							buffer = transformData(soundDataRetriever.getSoundData(sound.getData()), sound.getLvolume(), sound.getRvolume());
-						}
-
-						dataLine.write(buffer, 0, buffer.length);
-
-						// stop playing
-						dataLine.drain();
-						dataLine.stop();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-
-			} catch (InterruptedException e) {
-				// exit
-			} catch (LineUnavailableException e) {
-				e.printStackTrace();
-			}
-		}
+	public void close() {
+		sources.forEach(AL10::alDeleteSources);
+		sounds.values().forEach(AL10::alDeleteBuffers);
+		ALC10.alcDestroyContext(context);
+		ALC10.alcCloseDevice(device);
 	}
 
 	@Override
