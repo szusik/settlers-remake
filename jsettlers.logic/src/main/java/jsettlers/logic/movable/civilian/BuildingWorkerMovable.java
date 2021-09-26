cf01a4b8ce9ca3bef8a1fc08b87bbfa4ce7d8776
Paul Wedeck <paulwedeck@gmail.com>
2021-09-26 11:25:25 +0200

    add lambda based input/output stack methods

--- jsettlers.logic/src/main/java/jsettlers/logic/movable/civilian/BuildingWorkerMovable.java
+++ jsettlers.logic/src/main/java/jsettlers/logic/movable/civilian/BuildingWorkerMovable.java
@@ -76,19 +76,41 @@ public class BuildingWorkerMovable extends CivilianMovable implements IBuildingW
 	}
 
 	protected static <T extends BuildingWorkerMovable> Node<T> goToOutputStack(EMaterialType outputMaterial, IBooleanConditionFunction<T> pathStep) {
-		return goToPos(mov -> mov.getOutputStackPosition(outputMaterial), pathStep);
+		return goToOutputStack(mov -> outputMaterial, pathStep);
+	}
+
+	protected static <T extends BuildingWorkerMovable> Node<T> goToOutputStack(IEMaterialTypeSupplier<T> outputMaterial, IBooleanConditionFunction<T> pathStep) {
+		return goToPos(mov -> mov.getOutputStackPosition(outputMaterial.apply(mov)), pathStep);
 	}
 
 	protected static <T extends BuildingWorkerMovable> Node<T> goToInputStack(EMaterialType outputMaterial, IBooleanConditionFunction<T> pathStep) {
-		return goToPos(mov -> mov.getInputStackPosition(outputMaterial), pathStep);
+		return goToInputStack(mov -> outputMaterial, pathStep);
+	}
+
+	protected static <T extends BuildingWorkerMovable> Node<T> goToInputStack(IEMaterialTypeSupplier<T> outputMaterial, IBooleanConditionFunction<T> pathStep) {
+		return goToPos(mov -> mov.getInputStackPosition(outputMaterial.apply(mov)), pathStep);
 	}
 
 	protected static <T extends BuildingWorkerMovable> Node<T> outputStackNotFull(EMaterialType outputMaterial) {
 		return condition(mov -> mov.grid.canPushMaterial(mov.getOutputStackPosition(outputMaterial)));
 	}
 
+	protected static <T extends BuildingWorkerMovable> Node<T> outputStackNotFull(IEMaterialTypeSupplier<T> outputMaterial) {
+		return condition(mov -> {
+			EMaterialType mat = outputMaterial.apply(mov);
+			return mov.grid.canPushMaterial(mov.getOutputStackPosition(mat));
+		});
+	}
+
 	protected static <T extends BuildingWorkerMovable> Node<T> inputStackNotEmpty(EMaterialType inputMaterial) {
-		return condition(mov -> mov.grid.canTakeMaterial(mov.getInputStackPosition(inputMaterial), inputMaterial));
+		return inputStackNotEmpty(mov -> inputMaterial);
+	}
+
+	protected static <T extends BuildingWorkerMovable> Node<T> inputStackNotEmpty(IEMaterialTypeSupplier<T> inputMaterial) {
+		return condition(mov -> {
+			EMaterialType mat = inputMaterial.apply(mov);
+			return mov.grid.canTakeMaterial(mov.getInputStackPosition(mat), mat);
+		});
 	}
 
 	protected static <T extends BuildingWorkerMovable> Node<T> isAllowedToWork() {
