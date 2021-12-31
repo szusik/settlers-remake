83e18f24fbd7687c55c04b8c5e18e2c9e6aa637a
Paul Wedeck <paulwedeck@gmail.com>
2021-12-31 15:13:47 +0100

    some StartLanServerButton cleanup

--- jsettlers.main.swing/src/main/java/jsettlers/main/swing/menu/multiplayer/lan/StartLanServerButton.java
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/menu/multiplayer/lan/StartLanServerButton.java
@@ -1,15 +1,10 @@
 package jsettlers.main.swing.menu.multiplayer.lan;
 
-import jsettlers.graphics.localization.Labels;
 import jsettlers.network.infrastructure.log.Logger;
 import jsettlers.network.server.GameServerThread;
 import jsettlers.network.server.match.EPlayerState;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
 import javax.swing.JToggleButton;
-import javax.swing.border.TitledBorder;
-import java.awt.FlowLayout;
 import java.io.IOException;
 import java.util.function.Consumer;
 
@@ -20,7 +15,6 @@ public class StartLanServerButton extends JToggleButton {
 	private final Consumer<Boolean> serverActive;
 
 	public StartLanServerButton(Logger logger, Consumer<Boolean> serverActive) {
-		super("server active");
 		this.serverActive = serverActive;
 		this.logger = logger;
 
