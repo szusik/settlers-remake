655e173efc61866972e608af910f260eb36c0ed0
Paul Wedeck <paulwedeck@gmail.com>
2022-01-07 22:55:13 +0100

    add more materials to production log, add third argument

--- productionStats/script.plot
+++ productionStats/script.plot
@@ -1,4 +1,4 @@
-set terminal png size 1000,1000
+set terminal png size 500,500
 set output ARG1.'.png'
 set datafile separator ','
-plot ARG1.'-'.ARG2.'.log' with lines, ARG1.'-'.ARG3.'.log' with lines
+plot ARG1.'-'.ARG2.'.log' with lines, ARG1.'-'.ARG3.'.log' with lines, ARG1.'-'.ARG4.'.log' with lines
--- productionStats/script.sh
+++ productionStats/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for x in CROP          IRONORE          SPEAR          AXE          SCYTHE          MEAT          PLANK          WATER          SAW          NO_MATERIAL          FISHINGROD          BREAD          FISH          GOLDORE          HAMMER          GOLD          COAL          WINE          FLOUR          IRON          PIG          PICK          BOW          TRUNK          STONE          BLADE          ; do
-gnuplot -c script.plot $x $1 $2
+for x in CROP IRONORE SPEAR AXE SCYTHE MEAT PLANK WATER SAW NO_MATERIAL FISHINGROD BREAD FISH GOLDORE HAMMER GOLD COAL WINE FLOUR IRON PIG PICK BOW TRUNK STONE BLADE GEMS GUN_POWDER HONEY KEG LIQUOR MEAD RICE SULFUR SWORD; do
+gnuplot -c script.plot $x $1 $2 $3
 done
