20ce35d3eb12de7fe9580fb6d34e2363c0d77485
Paul Wedeck <paulwedeck@gmail.com>
2022-01-29 19:14:55 +0100

    use correct start resources in swing single player game

--- jsettlers.main.swing/src/main/java/jsettlers/main/swing/menu/joinpanel/JoinGamePanel.java
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/menu/joinpanel/JoinGamePanel.java
@@ -236,7 +236,8 @@ public class JoinGamePanel extends BackgroundPanel {
 					})
 					.toArray(PlayerSetting[]::new);
 
-			InitialGameState initialGameState = new InitialGameState(playerSlots.get(0).getSlot(), playerSettings, randomSeed);
+			MapStartResourcesUIWrapper selected = (MapStartResourcesUIWrapper) startResourcesComboBox.getSelectedItem();
+			InitialGameState initialGameState = new InitialGameState(playerSlots.get(0).getSlot(), playerSettings, randomSeed, selected.getStartResources());
 			JSettlersGame game = new JSettlersGame(mapLoader, initialGameState);
 			IStartingGame startingGame = game.start();
 			settlersFrame.showStartingGamePanel(startingGame);
